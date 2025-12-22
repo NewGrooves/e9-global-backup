bc01bd3a80581a68ab6278538c2413fc29e75112
NewGrooves <sam@zenwarriortraining.com>
2025-12-22 12:34:14 -0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,29 @@
-
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { HelmetProvider } from 'react-helmet-async';
-// import GoogleAnalytics from "@/components/GoogleAnalytics";
+import { HelmetProvider } from "react-helmet-async";
+
 import { OrganizationSchema, WebsiteSchema } from "@/components/StructuredData";
-// import CacheBreaker from "@/components/CacheBreaker";
 import ErrorBoundary from "@/components/ErrorBoundary";
+
 import IndexNew from "./pages/IndexNew";
 import Home2 from "./pages/Home2";
+import HomeStaging from "./pages/HomeStaging";
+
 import About from "./pages/About";
 import TheTeam from "./pages/TheTeam";
 import HowItWorks from "./pages/HowItWorks";
 import Beeep from "./pages/Beeep";
 import BeeepStagingPage from "./pages/BeeepStagingPage";
-import HomeStagingPage from "./pages/HomeStagingPage";
 import BookDemo from "./pages/BookDemo";
 import VideoPage from "./pages/VideoPage";
-
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
 
 const App = () => {
-  console.log('App component rendering');
-  
   return (
     <ErrorBoundary>
       <QueryClientProvider client={queryClient}>
@@ -36,21 +33,35 @@ const App = () => {
             <Sonner />
             <OrganizationSchema />
             <WebsiteSchema />
+
             <BrowserRouter>
               <Routes>
+                {/* Primary homepage */}
                 <Route path="/" element={<IndexNew />} />
+
+                {/* Legacy / alternate home routes */}
                 <Route path="/home-v2024" element={<Home2 />} />
                 <Route path="/home-2025" element={<Home2 />} />
                 <Route path="/home-fresh" element={<Home2 />} />
+
+                {/* New homepage staging */}
+                <Route path="/home-staging" element={<HomeStaging />} />
+
+                {/* Site pages */}
                 <Route path="/about" element={<About />} />
                 <Route path="/the-team" element={<TheTeam />} />
                 <Route path="/how-it-works" element={<HowItWorks />} />
+
+                {/* BEEEP */}
                 <Route path="/beeep" element={<Beeep />} />
                 <Route path="/beeep-staging" element={<BeeepStagingPage />} />
-                 <Route path="/home-staging" element={<HomeStagingPage />} />
+
+                {/* Other */}
                 <Route path="/book-demo" element={<BookDemo />} />
-<Route path="/video" element={<VideoPage />} />
-<Route path="*" element={<NotFound />} />
+                <Route path="/video" element={<VideoPage />} />
+
+                {/* Catch-all */}
+                <Route path="*" element={<NotFound />} />
               </Routes>
             </BrowserRouter>
           </TooltipProvider>
