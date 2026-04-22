71a6ef54a8cdc5d5acec513e958efb289d6b5b5b
NewGrooves <sam@zenwarriortraining.com>
2026-04-22 16:58:54 -0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,18 @@
 // src/App.tsx
-
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { HelmetProvider } from "react-helmet-async";
-
 import { OrganizationSchema, WebsiteSchema } from "@/components/StructuredData";
 import ErrorBoundary from "@/components/ErrorBoundary";
-
 import Home2 from "./pages/Home2";
 import HomeStaging from "./pages/HomeStaging";
-
 import TheTeam from "./pages/TheTeam";
-
 import BeeepStagingPage from "./pages/BeeepStagingPage";
+import BeeepStagingPageV2 from "./pages/BeeepStagingPageV2";
 import SecurityTech from "./pages/SecurityTech";
-
 import BookDemo from "./pages/BookDemo";
 import VideoPage from "./pages/VideoPage";
 import NotFound from "./pages/NotFound";
@@ -34,18 +29,15 @@ const App = () => {
             <Sonner />
             <OrganizationSchema />
             <WebsiteSchema />
-
             <BrowserRouter>
               <Routes>
                 {/* Primary homepage */}
-                <Route path="/" element={<HomeStaging />} />
-
+                <Route path="/" element={<BeeepStagingPage />} />
                 {/* Legacy / alternate home routes */}
                 <Route path="/home-v2024" element={<Home2 />} />
                 <Route path="/home-2025" element={<Home2 />} />
                 <Route path="/home-fresh" element={<Home2 />} />
                 <Route path="/home-staging" element={<HomeStaging />} />
-
                 {/* Deactivated pages */}
                 <Route path="/about" element={<Navigate to="/" replace />} />
                 <Route path="/how-it-works" element={<Navigate to="/" replace />} />
@@ -53,18 +45,16 @@ const App = () => {
                   path="/how-it-works-staging"
                   element={<Navigate to="/" replace />}
                 />
-
                 {/* Team */}
                 <Route path="/the-team" element={<TheTeam />} />
-
                 {/* E9 Engagement Platform */}
                 <Route path="/platform" element={<BeeepStagingPage />} />
                 <Route path="/beeep" element={<Navigate to="/platform" replace />} />
                 <Route path="/beeep-staging" element={<Navigate to="/platform" replace />} />
-
+                {/* V2 Experimental Page */}
+                <Route path="/v2" element={<BeeepStagingPageV2 />} />
                 {/* Security Tech (canonical) */}
                 <Route path="/security-tech" element={<SecurityTech />} />
-
                 {/* Redirect legacy URLs */}
                 <Route
                   path="/security-tech-stack"
@@ -74,11 +64,9 @@ const App = () => {
                   path="/security-tech-stack-staging"
                   element={<Navigate to="/security-tech" replace />}
                 />
-
                 {/* Other */}
                 <Route path="/book-demo" element={<BookDemo />} />
                 <Route path="/video" element={<VideoPage />} />
-
                 {/* Catch-all */}
                 <Route path="*" element={<NotFound />} />
               </Routes>
@@ -91,4 +79,3 @@ const App = () => {
 };
 
 export default App;
-
