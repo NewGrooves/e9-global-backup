e63d2455fcc1af7ca42c1d6c62fb4fe9ea902343
NewGrooves <sam@zenwarriortraining.com>
2026-04-22 15:41:25 -0700

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,7 +7,7 @@ const Footer = () => {
         <div className="flex flex-col sm:flex-row justify-between items-center space-y-4 sm:space-y-0">
           {/* Copyright */}
           <div className="text-gray-700 text-sm">
-            ¬© 2026 E9 Global, INC. All rights reserved.
+            &copy; 2026 E9 Global, INC. All rights reserved.
           </div>
 
           {/* Footer Navigation */}
@@ -47,4 +47,3 @@ const Footer = () => {
 };
 
 export default Footer;
-
